9e4e17f752b4d5c5ed72839c37268021f4c67d82
Lucas <lucas@pamorana.net>
2024-05-08 18:52:49 +0200

    added copyright notice

--- src/modbus.c
+++ src/modbus.c
@@ -3,9 +3,21 @@
  * modbus.c
  * lucas@pamorana.net (2024)
  *
- * Gather metrics from ABB Energy Meters (A43),
- * using a custom Raspberry Pi HAT, and post
- * the data to an InfluxDB server.
+ * Gather metrics from ABB Energy Meters (A43), using a custom Raspberry Pi HAT,
+ * and post the data to an InfluxDB server.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <stddef.h>
